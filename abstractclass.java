d2a1b7fae5fc28f1fff6c1754ea948d68d42a7d9
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-01 11:23:13 +0530

    Add files via upload

---
+++ abstractclass.java
@@ -0,0 +1,33 @@
+﻿package com.company;
+import java.util.Scanner;
+abstract class Marking
+{
+    public abstract void Mark();
+    public static void f(){
+        System.out.println("Enter the Marks");
+    }
+}
+class Marks extends Marking{
+    public void Mark()
+    {
+        Scanner in = new Scanner(System.in);
+        int n,i,sum=0;
+        n = in.nextInt();
+        int []a= new int[100];
+        for(i=0;i<n;i++)
+        {
+            a[i]= in.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            sum = sum+a[i];
+        }
+        System.out.println("Sum is "+" "+sum+" "+"and the average is "+ sum/n);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+       Marks m = new Marks();
+       m.Mark();
+    }
+}
